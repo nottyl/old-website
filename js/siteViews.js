c38dea1c5f7fd08f2acc3dc62d99ab9472914e8b
teewhy <71718049+nottyl@users.noreply.github.com>
2022-12-27 15:24:55 +0800

    Update blog covers

--- js/siteViews.js
+++ js/siteViews.js
@@ -1,3 +1,7 @@
 var siteViews = function(response) {
     document.getElementById('visits').innerText = response.value;
 }
+
+var midnightsViews = function(response) {
+    document.getElementById('hits').innerText = response.value;
+}
